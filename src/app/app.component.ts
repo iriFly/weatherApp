9fdc3222c557bb94b16d2193d46ff80991f7bd3f
Irina Afanassenko <irina.afanassenko@uzleuven.be>
2022-12-19 14:45:19 +0100

    weather api added together w weather services and model json. work well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather_app';
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Leuven')
+    .subscribe({
+      next: (response) => {
+        this. weatherData = response;
+        console.log(response);
+      }
+    });
+
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '8595db04b4msheb0a2227f7ee9b6p15c536jsnc7619a5e1100',
+
+
+ };
